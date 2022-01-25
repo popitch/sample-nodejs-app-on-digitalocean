46438a9842fc675c44ae3bf46e41382b43939241
Popitch <popitch77@gmail.com>
2022-01-25 10:39:55 +0300

    ? auth: async () => {

--- db.js
+++ db.js
@@ -1,12 +1,11 @@
+const { Sequelize, DataTypes } = require('sequelize');
+
 const 
     dbConnString = process.env.DATABASE_URL + "&ssl=true",
     dbConnURL = new URL(dbConnString),
     
-    dbConnCert = process.env[ "DB_CERTIFICATE" ], // require('fs').readFileSync(__dirname + '/ca-certificate.crt').toString(),
-    //dbConnCertBase64 = dbConnCert
-    //    .replace(/^-*BEGIN CERTIFICATE-*\s*(.*)\s*-*END CERTIFICATE-*\s*$/, '$1'),
+    DB_CERTIFICATE = process.env[ "DB_CERTIFICATE" ],
     
-    Sequelize = require('sequelize'),
     sequelize = new Sequelize(
         dbConnURL.pathname.substr(1), // database, "/defaultdb" => "defaultdb" ;)
         dbConnURL.username,
@@ -22,41 +21,32 @@ const
                 ssl: {
                     require: true,
                     rejectUnauthorized: false,
-                    ca: dbConnCert,
-                    //caBase64Decoded: new Buffer(dbConnCert, 'base64').toString('ascii'),
+                    ca: DB_CERTIFICATE
                 },
-            },
-            /*
-            ssl: {
-                require: true,
-                rejectUnauthorized: false,
-                ca: dbConnCert,
-                //caBase64Decoded: new Buffer(dbConnCert, 'base64').toString('ascii'),
-            },
-            */
+            }
         }
     );
 
 console.log('connOpts', JSON.stringify(connOpts, null, 4));
 
 
-// connect
+// connected
 let connectedSeq;
-
 
 
 // exports
 module.exports = {
     auth: async () => {
-        console.log('Connect to db...', process.env.DATABASE_URL);
+        console.log('DB...', process.env.DATABASE_URL);
         
-        return connectedSeq = connectedSeq || await sequelize.authenticate()
-            .catch(console.warn.bind(console, 'Connect to db...', 'Unable to connect to the db'))
-            .then(() => {
-                console.log('Connect to db...', 'Connection has been established successfully.');
+        connectedSeq = connectedSeq || await sequelize.authenticate();
+            //.catch(console.warn.bind(console, 'DB...', 'Unable to connect to the db'))
+            //.then(() => {
+            //    console.log('DB...', 'Connection has been established successfully.');
                 
                 // test conn
                 
-            });
+            //});
+        return connectedSeq;
     }
 };
