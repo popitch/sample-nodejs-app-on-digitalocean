1dd49d7d0b745d889edbd5831682c75e9c9afad0
Popitch <popitch77@gmail.com>
2022-01-26 17:13:55 +0300

    static, for /cached/*.json

--- app.js
+++ app.js
@@ -174,9 +174,6 @@ const fs = require('fs'),
 
 var app = express();
 
-// my static, for /cached/*.json
-app.use('/cached', express.static(path.join(__dirname, 'public/cached')));
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -187,7 +184,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+// my static, for /cached/*.json
+app.use('/cached', express.static(path.join(__dirname, 'public/cached')));
+
+app.use('/*', indexRouter);
 // app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
