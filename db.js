aba676805379ff282c2b37e8c436057af6e221da
Popitch <popitch77@gmail.com>
2022-02-17 21:37:40 +0300

    addColumn('exchangers', 'description

--- db.js
+++ db.js
@@ -79,7 +79,7 @@ sequelize.define('AggUser', schema.AggUser.fields, {
 connThen(async (db) => {
     const queryInterface = db.getQueryInterface();
     
-    await queryInterface.addColumn('Exchanger', 'description', schema.Exchanger.fields.description);
+    await queryInterface.addColumn('exchangers', 'description', schema.Exchanger.fields.description);
     /*
     await queryInterface.dropTable('exchangers', { onDelete: 'cascade' });
     await queryInterface.createTable('exchangers', schema.Exchanger.fields);
