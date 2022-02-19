1986e85ef55fcaa77be18caebbddca45a110f955
Popitch <popitch77@gmail.com>
2022-02-19 11:49:21 +0300

    button')[0].style.visibility

--- public/admin/js/exchanger_edit.js
+++ public/admin/js/exchanger_edit.js
@@ -8,12 +8,12 @@ $(() => {
 function setButtonVisibility() {
     const current = $('input, textarea').get()
         .reduce((values, input) => {
-            values[input.name] = $(input).is(':checkbox') ? !!$(input).is(':checked') : $(input).val();
+            values[input.name] = $(input).is(':checkbox') ? !! $(input).is(':checked') : $(input).val();
             return values;
         }, {}),
-        isIdent = _.all(_.keys(current), key => !current[key] && !exch[key] || current[key] == exch[key]);
+        isIdent = _.all(_.keys(current), key => (! current[key] && ! exch[key]) || current[key] == exch[key]);
     
     console.log('isIdent:', isIdent, ', current:', current, ', exch:', exch);
     
-    $('button').toggle(! isIdent);
+    $('button')[0].style.visibility = isIdent ? 'hidden' : 'visible';
 }
