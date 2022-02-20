bbca33f276f5c298179a7741a9fefa7bd38ea621
Popitch <popitch77@gmail.com>
2022-02-20 15:59:38 +0300

    user login allowNull: false

--- db.schema.js
+++ db.schema.js
@@ -20,7 +20,7 @@ module.exports = {
             login: {
                 type: DataTypes.STRING(32),
                 defaultValue: null,
-                allowNull: true,
+                allowNull: false,
             },
             
             passwd: {
