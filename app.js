1109c3f1c9e1c361a8aff2682b983154f637c81a
Popitch <popitch77@gmail.com>
2022-02-20 12:02:08 +0300

    ~ setters

--- app.js
+++ app.js
@@ -187,12 +187,10 @@ app.post('/admin/table/exchangers/:id', async (req, res) => {
                 console.log('... exch.setDataValue(', key, ', ', req.body[key], ') ...');
                 exch.setDataValue(key, req.body[key]);
             }
-            /*
-            if (_.has(req.body, 'key')) {
-                console.log('... exch[', key, '] = ', req.body[key], '');
+            //if (_.has(req.body, 'key')) {
                 exch[key] = req.body[key];
-            }
-            */
+                console.log('... exch[', key, '] = ', req.body[key], '');
+            //}
         });
         
         console.log('save exch ...', exch.id);
