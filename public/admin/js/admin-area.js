f8324ae43d28dce26fffc4e284418a31aeda667a
popitch <popitch@ya.ru>
2022-02-22 17:38:13 +0300

    admin ~ css

--- public/admin/js/admin-area.js
+++ public/admin/js/admin-area.js
@@ -1,1 +1,2 @@
-$('a[href="' + $.escapeSelector(location.pathname) + '"]').addClass('admin-area-link_current');
+// highlight for current path link
+$(() => $('a[href="' + $.escapeSelector(location.pathname) + '"]').addClass('admin-area-link_current'));
