3e23fe47b346601a2155978b5b6ae67d17c0c07f
popitch <popitch@ya.ru>
2022-04-11 16:29:50 +0300

    dropTable('exchangers before initial update

--- db.js
+++ db.js
@@ -87,6 +87,8 @@ connThen(async (db) => {
     /** Setup
      * Initial exchanger list
      */
+    await queryInterface.dropTable('exchangers', { onDelete: 'cascade' });
+    
     const Exchanger = sequelize.models.Exchanger,
         presentedExchangers = await Exchanger.findAll({ where: { xmlVerified: true } }),
         INITIAL_EXCHANGERS = JSON.parse(process.env[ "INITIAL_EXCHANGERS" ]);
