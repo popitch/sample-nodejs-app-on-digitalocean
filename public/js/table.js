41fe3c578a9e1a66469a87815ffcc5e7e58efff2
popitch <popitch@ya.ru>
2022-04-17 13:32:45 +0300

    //log

--- public/js/table.js
+++ public/js/table.js
@@ -321,7 +321,7 @@ const
                 more();
             })
             .catch(e => {
-                console.warn('wow, error', e);
+                //console.warn('wow, error', e);
                 exchangeRates.rates([]);
                 more();
             });
