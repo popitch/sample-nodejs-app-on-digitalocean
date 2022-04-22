d27974cdc3b620ccb0846c81382aad738042ee26
popitch <popitch@ya.ru>
2022-04-22 12:00:49 +0300

    dynamic currency labels

--- public/js/rate-utils.js
+++ public/js/rate-utils.js
@@ -122,6 +122,18 @@ function rateExchangerExchangeUrl(rate, exch) {
     return url;
 }
 
+function numberWithSpaces(x, fixed) {
+    x = Number(x);
+    fixed = Math.min(Math.max(fixed, 0), 8);
+    
+    let parts = (fixed ? x.toFixed(fixed) : x.toString()).split(".");
+    
+    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+    return parts.map(p => p).join(".");
+    //return parts.map(p => p.replace(/\B(?=(\d{3})+(?!\d))/g, " ")).join(".");
+}
+
+
 // module.exports for SSR
 if (typeof module !== 'undefined') {
     module.exports = {
@@ -136,8 +148,10 @@ if (typeof module !== 'undefined') {
         pairsToCurrenciesFrom: pairsToCurrenciesFrom,
         pairsToCurrenciesTo: pairsToCurrenciesTo,
         
+        // utils
         rateExchangerExchangeUrl: rateExchangerExchangeUrl,
+        numberWithSpaces: numberWithSpaces,
         
-        currencyFN: (symbol) => CURRENCY_NAME_BY_SYMBOL[symbol] || symbol,
+        currencyToName: (symbol) => CURRENCY_NAME_BY_SYMBOL[symbol] || symbol,
     };
 }
